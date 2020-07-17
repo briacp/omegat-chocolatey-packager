c7702f8076b002f1d21409f37514fe55d923f30d
Briac Pilpré <briacp@gmail.com>
2020-07-17 13:19:26 +0200

    Added uninstall script to be very silent.

---
+++ tools/chocolateyUninstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageName = 'omegat'
+
+if ( test-path "C:\Program Files (x86)" ) {
+	$unpath = "C:\Program Files (x86)\OmegaT\unins000.exe"
+} else {
+	$unpath = "C:\Program Files\OmegaT\unins000.exe"
+}
+
+Uninstall-ChocolateyPackage $packageName 'exe' '/VERYSILENT' "$unpath" -validExitCodes @(0)
